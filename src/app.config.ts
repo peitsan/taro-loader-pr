c30fa2d707c54e65fc1dbd9038d81243d9895aef
‘peitsan’ <reality3iiru@163.com>
2023-01-21 11:43:40 +0800

    feat: 添加路由分包管理&添加底部Tarbar导航

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,28 +1,21 @@
 export default defineAppConfig({
   pages: [
-    // 系统登录
-    'pages/login/index',
-    // 主页
-    'pages/home/index/index',
+    /* 
+    主页(可以不做)
+  */
+    // 'pages/home/index/index',
     /* 
     工程管理
   */
     'pages/home/projectManage/index',
-    // 工程总览
-    'pages/home/projectManage/projectOverview/index',
-    // 工程进度
-    'pages/home/projectManage/projectOverview/fatherProjectProgress/index',
-    // 三项清单查阅
-    'pages/home/projectManage/projectOverview/projectList/index',
-    // 三项清单审批
-    'pages/home/projectManage/threeListAudit/index',
-    // 工程审核
-    // 工程移交
-    // 清单审核
-    // 上报审核
+    /* 
+    人员管理
+  */
+    'pages/home/personManage/index',
     /* 
     项目团队管理
   */
+    'pages/home/projectTeamManage/index',
     /* 
     典型经验
   */
@@ -32,12 +25,76 @@ export default defineAppConfig({
   */
     'pages/home/managerManage/index',
   ],
-  entryPagePath: 'pages/home/typicalExperience/index',
-  // entryPagePath: 'pages/login/index',
+  subpackages: [
+    {
+      root: 'pages/home/projectManage/projectOverview',
+      // 工程总览
+      pages: [
+        'index',
+        // 工程进度
+        'fatherProjectProgress/index',
+        // 三项清单查阅
+        'projectList/index',
+      ],
+    },
+    {
+      root: 'pages/home/projectManage/threeListAudit',
+      // 三项清单审批
+      pages: ['index'],
+    },
+    {
+      root: 'pages/home/projectManage/projectHandOver',
+      // 工程移交
+      pages: ['index'],
+    },
+    {
+      root: 'pages/home/projectManage/projectAudit',
+      // 工程审核
+      pages: [
+        'index',
+        // 工程审核
+        // 清单审核
+        // 上报审核
+      ],
+    },
+    {
+      // 系统登录
+      root: 'pages/login',
+      pages: ['index'],
+    },
+  ],
+  // entryPagePath: 'pages/home/typicalExperience/index',
+  entryPagePath: 'pages/login/index',
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
+    backgroundTextStyle: 'dark',
+    navigationBarBackgroundColor: '#0EB295',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black',
+  },
+  tabBar: {
+    color: '#8a8a8a',
+    selectedColor: '#18A456',
+    backgroundColor: '#FBFBFB',
+    borderStyle: 'black',
+    list: [
+      {
+        pagePath: 'pages/home/projectManage/index',
+        text: '工程管理',
+        iconPath: './assets/icon/projectAdmin.png',
+        selectedIconPath: './assets/icon/projectAdmin-active.png',
+      },
+      {
+        pagePath: 'pages/home/projectTeamManage/index',
+        text: '团队管理',
+        iconPath: './assets/icon/teamAdmin.png',
+        selectedIconPath: './assets/icon/teamAdmin-active.png',
+      },
+      {
+        pagePath: 'pages/home/typicalExperience/index',
+        text: '典型经验',
+        iconPath: './assets/icon/specialExperience.png',
+        selectedIconPath: './assets/icon/specialExperience-active.png',
+      },
+    ],
   },
 });
