8767deb746cbdaf95ed8361218b68b9618dd20fc
peitsan <reality3iiru@163.com>
2023-02-28 16:25:08 +0800

    feat: 团队管理鉴权

--- src/pages/home/projectTeamManage/index.tsx
+++ src/pages/home/projectTeamManage/index.tsx
@@ -1,9 +1,11 @@
+import Taro from '@tarojs/taro';
 import { AtTag } from 'taro-ui';
 import { View } from '@tarojs/components';
 import TeamList from './teamList/index';
 import styles from './index.module.less';
 
 const projectTeamManage: React.FC = () => {
+  const permission = Taro.getStorageSync('permission');
   return (
     <>
       <View className={styles.top}>
@@ -11,9 +13,15 @@ const projectTeamManage: React.FC = () => {
           团队管理
         </AtTag>
       </View>
-      <View>
-        <TeamList />
-      </View>
+      {permission === 'manager' ? (
+        <View>
+          <TeamList />
+        </View>
+      ) : (
+        <View style={{ margin: '50px auto', textAlign: 'center' }}>
+          该模块只对项目经理开放
+        </View>
+      )}
     </>
   );
 };
